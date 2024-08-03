9477007d83d550ec2380627168f005ada9d8d96f
Chris Kennedy <chris94kennedy@gmail.com>
2024-08-03 22:27:50 +0100

    ++

--- typescript/src/parse/tagNums.ts
+++ typescript/src/parse/tagNums.ts
@@ -7,34 +7,51 @@ import { DicomErrorType } from "../globalEnums.js";
  * responsibility to pass in the subarray that they determine
  * to be the 4 bytes representing the tag (via cursor walking).
  * @param buf
- * @returns string `(${string},${string})`
+ * @returns string
  */
 export function decodeTagNum(buf: Buffer): `(${string},${string})` {
    if (buf.length !== 4) {
-      throw new DicomError({
-         errorType: DicomErrorType.PARSING,
-         message: `decodeTagNum() expects a 4byte buffer`,
-         buffer: buf,
-      });
+      return throwBadBufferLength(buf);
    }
 
-   const decode = (cursor: number): string =>
-      buf
+   const decode = (cursor: number): string => {
+      return buf
          .readUInt16LE(cursor) // group nums are always 2 bytes
          .toString(16) // hexes are base 16 so pass radix 16
          .padStart(4, "0"); // pad with 0s to make it 4 chars long
+   };
 
+   const isHexStr = (str: string): boolean => /^[0-9a-fA-F]{4}$/.test(str); // DICOM tags are always 4 hex chars
    const grp = decode(0);
    const el = decode(2);
-   const syntax = /^[0-9a-fA-F]{4}$/;
 
-   if (!syntax.test(grp) || !syntax.test(el)) {
-      throw new DicomError({
-         errorType: DicomErrorType.PARSING,
-         message: `decodeTag() decoded to an unexpected syntax:(${grp},${el})`,
-         buffer: buf,
-      });
+   if (!isHexStr(grp) || !isHexStr(el)) {
+      return throwBadHexPattern(buf, `(${grp},${el})`);
    }
 
    return `(${grp},${el})`;
+}
+
+/**
+ * Throw an error if the buffer length is not 4 bytes.
+ * @param buf
+ */
+function throwBadBufferLength(buf: Buffer): never {
+   throw new DicomError({
+      errorType: DicomErrorType.PARSING,
+      message: `decodeTagNum() expects a 4byte buffer`,
+      buffer: buf,
+   });
+}
+
+/**
+ * Throw an error if the buffer did not decode to a 4 hex character string.
+ * @param buf
+ */
+function throwBadHexPattern(buf: Buffer, str: string): never {
+   throw new DicomError({
+      errorType: DicomErrorType.PARSING,
+      message: `decodeTag() decoded to an unexpected hexPattern: ${str}`,
+      buffer: buf,
+   });
 }
