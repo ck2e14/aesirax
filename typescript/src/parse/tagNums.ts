65bd1c2c91bd03f48541ad2f05179acea1aef7b0
Chris Kennedy <chris94kennedy@gmail.com>
2024-08-03 21:37:03 +0100

    ++

--- typescript/src/parse/tagNums.ts
+++ typescript/src/parse/tagNums.ts
@@ -1,5 +1,11 @@
 import { DicomError } from "../error/dicomError.js";
 
+/**
+ * Pass in a 4 byte buffer and get back the tag as a string
+ * else throw a DicomError if unrecognised
+ * @param buf
+ * @returns
+ */
 export function decodeTagNum(buf: Buffer): `(${string},${string})` {
    if (buf.length !== 4) {
       throw new DicomError({
@@ -9,15 +15,25 @@ export function decodeTagNum(buf: Buffer): `(${string},${string})` {
       });
    }
 
-   const groupNumber = buf //
+   const groupNumber = buf
       .readUInt16LE(0) // group nums are always 2 bytes
-      .toString(16) // convert to hex representation (base 16)
+      .toString(16) // hexes are base 16 so pass radix 16
       .padStart(4, "0"); // pad with 0s to make it 4 chars long
 
    const elementNumber = buf //
-      .readUInt16LE(2)
+      .readUInt16LE(2) // we read the first 2 bytes so offset by those 2
       .toString(16)
       .padStart(4, "0");
+
+   const isPermissibleSyntax = /^[0-9a-fA-F]{4}$/;
+
+   if (!isPermissibleSyntax.test(groupNumber) || !isPermissibleSyntax.test(elementNumber)) {
+      throw new DicomError({
+         errorType: Errors.DicomErrorType.PARSING,
+         message: `decodeTag() expects a valid tag, got ${groupNumber},${elementNumber}`,
+         buffer: buf,
+      });
+   }
 
    return `(${groupNumber},${elementNumber})`;
 }
