b28605b96e84f519f7b1d8e931207c2c51e6156b
Chris Kennedy <chris94kennedy@gmail.com>
2024-08-06 01:51:55 +0100

    ++

--- typescript/src/error/initError.ts
+++ typescript/src/error/initError.ts
@@ -9,11 +9,13 @@ import { write } from "../logging/logQ.js";
 export class InitError extends Error {
    constructor(message: string, originalStack?: string) {
       super(message);
+
       this.name = "InitError";
+
       if (originalStack) {
-         // Append the original stack trace if it exists
          this.stack = `${this.stack}\nCaused by: ${originalStack}`;
       }
+
       write(`Error: ${this.name} - ${message} - ${this.stack}`, "ERROR");
    }
 
