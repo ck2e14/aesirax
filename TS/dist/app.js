129d5acf73ea0b02bb89c6768ccf044318dc0f14
Chris Kennedy <chris94kennedy@gmail.com>
2024-08-12 00:56:04 +0100

    now-handling-defined-length-SQs-as-well-as-undefined-length-SQs

--- TS/dist/app.js
+++ TS/dist/app.js
@@ -3,11 +3,11 @@ import { write } from "./logging/logQ.js";
 import { singleTheaded } from "./singlethreaded.js";
 import { readdirSync } from "node:fs";
 const testDirs = {
-    noNestedSQ_multipleItemsInsideSQ_undefinedLengthSQ_undefinedLengthItems: "../data/with_1-depthSQ_multiple_items_undefined_SQlen_undefinedItemLen",
-    turkey: "../data/turkey",
-    noNestedSQ_singleItemsInsideSQ_undefinedLengthSQ_undefinedLengthItems: "../data/with_1-depth_sequences_undefinedSQlen_undefinedItemlen",
-    nestedSQ_singleItemsInsideSQ_definedLengthSQ_definedLengthItems: "../data/Pi", // not working because haven't implemented handling for SQ's with defined lengths
-    x: "../data/x",
+    noNestedSQ_multipleItemsInsideSQ_undefinedLengthSQ_undefinedLengthItems: "../data/with_1-depthSQ_multiple_items_undefined_SQlen_undefinedItemLen", // 102 elements
+    turkey: "../data/turkey", // 51 elements
+    noNestedSQ_singleItemsInsideSQ_undefinedLengthSQ_undefinedLengthItems: "../data/with_1-depth_sequences_undefinedSQlen_undefinedItemlen", // 130 elements
+    nestedSQ_singleItemsInsideSQ_definedLengthSQ_definedLengthItems: "../data/Pi", //42 elements.
+    x: "../data/x", // 115 elements
 };
 /**
  * Main entry point for the application.
@@ -23,8 +23,18 @@ async function main(cfg) {
         write(`Starting up...`, "INFO");
     }
     await init();
-    cfg.targetDir = testDirs.nestedSQ_singleItemsInsideSQ_definedLengthSQ_definedLengthItems; // TODO when back from dogwalk - detect the end of defined length SQs that don't use sequence delmiters to end their sequences.
+    cfg.targetDir = testDirs.noNestedSQ_multipleItemsInsideSQ_undefinedLengthSQ_undefinedLengthItems; // TODO when back from dogwalk - detect the end of defined length SQs that don't use sequence delmiters to end their sequences.
     const fileCount = readdirSync(cfg.targetDir).length;
+    // alright sitrep
+    // think we are now supporting undefined and defined length SQs properly.
+    // however - we are not yet supporting LIFO stacking which is how we can
+    // handle nested SQs. For example the Pi SR has 2 defined length SQs in the top-most dataset.
+    // Both are properly traversed, but only the first one actually persists the data properly because
+    // it doesn't have a SQ nested within itself. The second one, ContentSequence(x0040a730), has a nested
+    // SQ in it, which trips the parser up and it puts it in the top-level dataset. Incidentally this overwrites
+    // the correctly handled first SQ because it is also a ContentSequence(x0040a730), and they end up sharing a key
+    // so it overwrites it :P
+    // handling nested sequencing is going to be the final piece of this SQ parsing puzzle. Fuck yeah!
     // await multiThreaded(cfg);
     await singleTheaded(cfg);
     // if (fileCount === 0) {
